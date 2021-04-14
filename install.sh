e1fbca434567849a7ace735081cecbedb6e86e73
Kevin Gwynn <kevin.gwynn@gmail.com>
2021-04-14 12:52:58 -0600

    Made install.sh much more fancy; creates backups and gives diffs when files are different; remove-backups.sh just deletes backup files created by install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,27 @@
 
 cwd="`pwd`"
 dest="$HOME"
+ext="dotfilesbak"
 
 echo "Installing from [$cwd] to [$dest]..."
+echo && echo
 cd $dest
 
 for f in $cwd/.[^.]*; do
 	basename=`basename $f`
 	if [ ! -L $f ] && [ ! -d $f ] && [[ "$basename" != ".gitignore" ]]; then
-		# TODO: Maybe make a backup of the destination file...
-		echo "$basename"
+		# TODO: Make creation of backups optional / maybe prompt if differences are found
+		echo "##### $basename #####"
+		# If destination file exists and they are different, create backup and display the differences
+		[[ -e $dest/$basename ]] && ( ! cmp $dest/$basename $f ) && cp $dest/$basename $dest/$basename.$ext && diff $dest/$basename $f
 		cp $f $dest
+		echo && echo
 	fi
-
 done
 
+echo "Backups saved:"
+ls -la $dest/.*.$ext 2>/dev/null
 
+echo 
+echo "To remove backups:"
+echo "  rm $dest/.*.$ext    OR    ./remove-backups.sh"
---
+++ remove-backups.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+dest="$HOME"
+ext="dotfilesbak"
+
+rm -fv $dest/.*.$ext
